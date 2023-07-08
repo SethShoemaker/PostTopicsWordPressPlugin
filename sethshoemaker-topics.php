fc2437f2f955028dc3f9c84832936e314561e293
Seth Shoemaker <Shoemakerseth2003@gmail.com>
2023-07-08 14:22:20 -0400

    init

---
+++ sethshoemaker-topics.php
@@ -0,0 +1,10 @@
+<?php 
+
+/*
+    Plugin Name: Blog Topics
+    Plugin URI: https://github.com/SethShoemaker/PostTopicsWordPressPlugin
+    Description: Allows you to declare different "topics" for posts.
+    Version: 1.0
+    Author: Seth Shoemaker
+    AuthorURI: https://github.com/SethShoemaker
+*/
